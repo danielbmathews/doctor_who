fdebd4f9ab6b814c7f07a71a9b89076604c2c65c
Daniel Mathews <danielbmathews@gmail.com>
2016-11-21 15:59:14 -0800

    Link parser now works

--- doctor_who.py
+++ doctor_who.py
@@ -1,5 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
+from urlparse import urljoin
+from bs4 import BeautifulSoup, SoupStrainer
 import pandas as pd
 
 
@@ -10,16 +11,21 @@ content = result.content
 
 soup = BeautifulSoup(content, 'lxml')  # Parse the HTML as a string
 html_table = soup.find_all('table')[0]  # Grab the first table
-episodes = pd.DataFrame(columns=['Episode','Production','Airdate'])
+episodes = pd.DataFrame(columns=['Episode', 'Link', 'Production', 'Airdate'])
 
 for row in html_table.find_all("tr"):
     cells = row.find_all("td")
-    if len(cells) == 3:
-        episodes = episodes.append({'Episode':cells[0].text,
-                                    'Production':cells[1].text,
-                                    'Airdate':cells[2].text.replace('\r\n', ' ')
-                                    },
-                                   ignore_index=True)
+    if len(cells) ==3 and cells[0].find(href=True):
+        Episode = cells[0].text
+        Link = urljoin(url, cells[0].find(href=True)['href'])
+        Production = cells[1].text
+        Airdate = cells[2].text.replace('\r\n', ' ')
+        episodes = episodes.append({'Episode': Episode,
+                                    'Link': Link,
+                                    'Production': Production,
+                                    'Airdate': Airdate
+                                     },
+                                     ignore_index=True)
 
-episodes = episodes.ix[3:]
+# episodes = episodes.ix[3:]
 print episodes
